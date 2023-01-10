388d7e1f027ac4058d917a6d757b1de1715fc4f3
kahyeokjin <hyeokjin977@gmail.com>
2023-01-10 23:09:24 +0900

    v1-test

---
+++ layouts/default.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <header class="title">
+        <nuxt-link class="logo" to="/">NuxtShoppingMaverick</nuxt-link>
+    </header>
+    <Nuxt/>
+  </div>
+
+</template>
+
+
+
+<style scoped>
+/* 헤더관련 스타일 */
+header {
+  height: 60px;
+  display: flex;
+  align-items: center;
+  padding: 0 0.5rem;
+}
+.logo {
+  font-weight: 900;
+}
+.logo:visited {
+  color: inherit;
+}
+</style>
---
+++ pages/Cart.vue
@@ -0,0 +1,73 @@
+<template>
+    <div class="container">
+      <h1 class="list-title">카트 페이지입니다!!</h1>
+      <CartList></CartList>
+      <div class="extra-panel">
+        <button>구매하기!</button>
+      </div>
+      <div class="cart-wrapper">
+        <button class="btn" @click="buyThing">구매하기</button>
+        <button class="btn" @click="clearThing">장바구니 비우기</button>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  // import { FETCH_CART_ITEMS} from '@/store'
+  import CartList from '~/components/CartList.vue'
+  
+  export default {
+    components: { CartList },
+    // async asyncData({ store }) {
+    //   await store.dispatch(FETCH_CART_ITEMS)
+    // },
+    methods: {
+      buyThing(){
+        console.log("buy")
+      },
+      clearThing(){
+        console.log("clear")
+      }
+    }
+  }
+  
+  </script>
+  
+  <style scoped>
+  .container {
+    margin: 2rem 10rem;
+  }
+  .list-title {
+    font-weight: 700;
+    font-size: 1.4rem;
+  }
+  .list-wrapper {
+    margin: 0.4rem 0;
+  }
+  .list-item {
+    display: flex;
+  }
+  .thumbnail {
+    width: 100px;
+    height: 100px;
+  }
+  .description {
+    padding: 2rem 1rem;
+  }
+  .extra-panel {
+    text-align: right;
+    padding: 0.2rem 0;
+  }
+  .cart-wrapper {
+    position: sticky;
+    float: right;
+    bottom: 50px;
+    right: 50px;
+  }
+  .cart-wrapper .btn {
+    display: inline-block;
+    height: 40px;
+    font-size: 1rem;
+    font-weight: 500;
+  }
+  </style>
